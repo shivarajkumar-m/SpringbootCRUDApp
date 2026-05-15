902a3e6ce193b72fdf06b1ec21813680ecfcdf21
shivarajkumar-m <shivarajkumarm47@gmail.com>
2026-05-15 14:58:03 +0530

    Refactor SpringbootDemoApplication structure and methods

--- src/main/java/com/ibm/SpringbootDemoApplication.java
+++ src/main/java/com/ibm/SpringbootDemoApplication.java
@@ -3,19 +3,34 @@ package com.ibm;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringbootDemoApplication extends SpringBootServletInitializer {
+public class SpringbootDemoApplication
+        extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
-	}
-	@Bean
-	public ModelMapper getMapper() {
-		return new ModelMapper();
-		
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(
+                SpringbootDemoApplication.class,
+                args
+        );
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+
+        return application.sources(
+                SpringbootDemoApplication.class
+        );
+    }
+
+    @Bean
+    public ModelMapper getMapper() {
+
+        return new ModelMapper();
+    }
 }
-
